413fd55775f47ef9c87ce962ded14ce6f952d81a
Nick Mueller <nickmueller@live.com>
2014-09-18 17:56:02 -0500

    Deal with doc

--- lib/stay_classy.rb
+++ lib/stay_classy.rb
@@ -25,8 +25,8 @@ module StayClassy
 	class Builder
 
 		######## StayClassy will only look within the rails/app/views directory. Leave the rest to the channel 9 news team ########
-		VIEWS_DIR = '/home/users/nmueller/Sandbox/tester/app/views/'.freeze #"#{ Rails.root.join( 'app', 'views' ) }"
-		VIEW_FILE_TYPES_REGEX = Regexp.union( /\.html\.erb$/ , /\.html$/ ) 
+		VIEWS_DIR = '/Users/nickmueller/K9feed/app/views'.freeze #"#{ Rails.root.join( 'app', 'views' ) }"
+		VIEW_FILE_TYPES_REGEX = Regexp.union( /\.html\.erb$/ , /\.html$/ ).freeze
 		DEFAULT_CLASS_TAGS = %w( h1 h2 h3 h4 h5 h6 div table p ).freeze
 		DEFAULT_ID_TAGS = %w( p span ).freeze
 
--- lib/stay_classy/stay_classy_process.rb
+++ lib/stay_classy/stay_classy_process.rb
@@ -11,27 +11,31 @@ module StayClassyProcess
 		printf "\nDirectories to make classy with prefix #{ prefix }: \n".colorize( :yellow )
 		view_dirs.map { |vd| printf "#{ vd }\n" }
 
-		# Send the right files to the nws team...
+		# Send the right files to the news team...
 		printf "\nFiles to make classy: \n".colorize( :yellow )
 		view_dirs.each do |vd|
 			Dir.foreach( vd ) do |file|
 				begin
+
 					if file.match( StayClassy::Builder::VIEW_FILE_TYPES_REGEX )
 
-						# Brian Fantana for Sex Pantherization
+						# Brian Fantana for Sex Pantherization.
+						# He gets @doc for the rest of the team.
 						brian_fantana( file )
 
-						# Champ Kind for sex and chicken
-						champ_kind( file )
+						# Champ Kind takes @doc for sex and chicken
+						champ_kind( @doc )
 
-						# What does he do with a toaster again?
-						brick_tamland( file )
+						# Puts @doc in a toaster...fantastic
+						brick_tamland( @doc )
 
-						# Maybe next time wear a bra
-						veronica_corningstone( file )
-
+						# Wears a bra next time
+						veronica_corningstone( @doc )
 					end
-				rescue
+
+				rescue Exception => e
+
+      		p "#{e}"
 
 				end
   		end
@@ -39,30 +43,34 @@ module StayClassyProcess
 
 	end
 
-	# Get the bits of real panther so you know it's good, even if they smell like a turd covered in burnt hair
+############### LADIES AND GENTLEMEN, NEWS TEAM 6 ###############
+
+	# Get the bits of real panther so you know it's good
 	def load_file( file )
-		@doc = Nokogiri::HTML::Document.parse( file )
+		@doc ||= Nokogiri::HTML::Document.parse( file )
 	end
 	alias :brian_fantana :load_file
 
 	# Slap some BBQ sauce on them elements and... Hawoooooooo, woo, woo, woo!!!!
 	def add_classes_to_file( file )
-		printf "#{ file }\n"
+		printf "#{ file } - Champ Kind, whammy!\n"
 	end
 	alias :champ_kind :add_classes_to_file
 
 	# Invite the ids to the pants party
 	def add_ids_to_file( file )
-		printf "#{ file }\n"
+		printf "#{ file } - Brick Tamland licking a toaster\n"
 	end
 	alias :brick_tamland :add_ids_to_file
 
 	# Thanks for listening, San Diego. And stay classy, San Diego. Thanks for listening, San Diego.
 	def save_new_file( file )
-		printf "#{ file }\n"
+		printf "#{ file } - Veronica Corningstone, signing off\n"
 	end
 	alias :veronica_corningstone :save_new_file
 
+#################################################################
+	
 	def baxter( file )
 		printf "#{ file }\n"
 	end

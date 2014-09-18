61a5c3b4cf4fa04588da2092fca3af24bc65dcb8
Nick Mueller <nmueller@iexposure.com>
2014-09-18 11:19:55 -0500

    Add comments and basic exception handling

--- lib/stay_classy.rb
+++ lib/stay_classy.rb
@@ -11,7 +11,7 @@
 #  \    |  |  |  |  |  ||     |    \     ||     ||  |  |\    |\    ||     |
 #   \___|  |__|  |__|__||____/      \____||_____||__|__| \___| \___||____/ 
 # 
-#                                            SAN Diego.....
+#                                            San Diego.....
 #
 #############################################################################
 
@@ -24,44 +24,48 @@ include StayClassyProcess
 module StayClassy
 	class Builder
 
-		######## StayClassy will only look within the rails/app/views directory ########
+		######## StayClassy will only look within the rails/app/views directory. Leave the rest to the channel 9 news team ########
 		VIEWS_DIR = '/home/users/nmueller/Sandbox/tester/app/views/'.freeze #"#{ Rails.root.join( 'app', 'views' ) }"
 		DEFAULT_CLASS_TAGS = %w( h1 h2 h3 h4 h5 h6 div table p ).freeze
 		DEFAULT_ID_TAGS = %w( p span ).freeze
 
 	  def initialize( options )
+	  	begin
+		  	# Handle user-specified directories here
+		  	if options[:dirs]
 
-	  	# Handle user-specified directories here
-	  	if options[:dirs]
+		  		directories = options[:dirs]
+		  		@specified_view_directories = []
+		  		
+		  		directories.each do |dir|
 
-	  		directories = options[:dirs]
-	  		@specified_view_directories = []
-	  		
-	  		directories.each do |dir|
+		  			# Filter out the not so classy characters. No scotch or ribs for them. Banish them to a whale's vagina
+		  			dir.gsub( /[^0-9a-z -_]/i, '' )
 
-	  			# Filter out the not so classy characters. No scotch or ribs for them. Banish them to a whale's vagina
-	  			dir.gsub( /[^0-9a-z -_]/i, '' )
+		  			# Build an array of valid directories. I don't know if you heard me counting, but I can do over 1,000.
+		  			if valid_dir?( dir ) 
+		  				@specified_view_directories << "#{ VIEWS_DIR }#{ dir }"
+		  			else
+		  				printf "\n#{ VIEWS_DIR }#{ dir } is not a valid directory. That's bush!".colorize( :red )
+		  			end
+		  		end
 
-	  			# Build an array of valid directories
-	  			if valid_dir?( dir ) 
-	  				@specified_view_directories << "#{ VIEWS_DIR }#{ dir }"
-	  			else 
-	  				printf "\n#{ VIEWS_DIR }#{ dir } is not a valid directory".colorize( :red )
-	  			end
-	  		end
+		  	else
+		  		@specified_view_directories = get_view_directories
+		  	end
 
-	  	else
-	  		@specified_view_directories = get_view_directories
-	  	end
+		  	######## Prefix for all classes/ids ########
+		  	options[:prefix] ? @prefix = set_prefix( options[:prefix].to_s ) : @prefix = 'classy_'
 
-	  	######## Prefix for all classes/ids ########
-	  	options[:prefix] ? @prefix = set_prefix( options[:prefix].to_s ) : @prefix = 'classy_'
-
-	  	if options[:tags]
-	  		######## Handle tags passed in ########
-	  	end
+		  	if options[:tags]
+		  		######## Handle tags passed in ########
+		  	end
+		  rescue Exception => e
+		  	raise e
+		  end
 	  end
 
+	  # Sixty percent of the time, valid directories will return true every time.
 	  def valid_dir?( directory )
 		  Dir.exists?( "#{ VIEWS_DIR }#{ directory }" )
 		end
@@ -79,10 +83,14 @@ module StayClassy
 	end
 end
 
-# Pass intended directories and class/id prefix to new
+# Pass intended directories and class/id prefix to new instance
 stay_classy = StayClassy::Builder.new( options = { :prefix => 'nmm',
 																					:dirs => %w( home ../shared users/sub_users )
 																				 } )
 
-# Send it off for processing if it all checks out
-StayClassyProcess.process( stay_classy ) unless stay_classy.instance_variable_get( :@specified_view_directories ).count == 0
+# If directories are there, send stay_classy off for processing. If not, go f*ck youself San Diego
+if stay_classy.instance_variable_get( :@specified_view_directories ).count == 0
+	printf "\n No directories found. If you were a man I would punch you! \n"
+else
+	StayClassyProcess.process( stay_classy )
+end
--- lib/stay_classy/stay_classy_process.rb
+++ lib/stay_classy/stay_classy_process.rb
@@ -2,17 +2,21 @@ require 'nokogiri'
 
 module StayClassyProcess
 
-	# Takes an instance of StayClassy
+	# Once you've built an instance of stay_classy, douse it with Sex Panther.
 	def process( stay_classy )
+
 		# if you need the views directory: StayClassy::Builder::VIEWS_DIR
 		prefix = stay_classy.instance_variable_get(:@prefix)
 		stay_classy.instance_variable_get(:@views_dir)
 		view_dirs = stay_classy.instance_variable_get(:@specified_view_directories)
 
 		printf "\n\n Directories to make classy with prefix #{ prefix }: \n".colorize( :yellow )
+
 		view_dirs.each do |view_dir|
 			printf "#{ view_dir }\n".colorize( :green )
 		end
+
+		# I want to be on those files.
 		doc = Nokogiri::HTML( '' )
   		# doc.css("div.to-replace").each do |div|
     # 		 "get_html_text" will obtain HTML from db. It can be anything, even another tags, tag groups etc.

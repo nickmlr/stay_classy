b562ac091e144812697e5dc1bcac182e2c5e9f44
Nick Mueller <nickmueller@live.com>
2014-09-18 17:27:01 -0500

    The news team

--- lib/stay_classy/stay_classy_process.rb
+++ lib/stay_classy/stay_classy_process.rb
@@ -11,13 +11,28 @@ module StayClassyProcess
 		printf "\nDirectories to make classy with prefix #{ prefix }: \n".colorize( :yellow )
 		view_dirs.map { |vd| printf "#{ vd }\n" }
 
-		# Send the right files to Brian Fantana for Sex Pantherization
+		# Send the right files to the nws team...
 		printf "\nFiles to make classy: \n".colorize( :yellow )
 		view_dirs.each do |vd|
 			Dir.foreach( vd ) do |file|
-				if file.match( StayClassy::Builder::VIEW_FILE_TYPES_REGEX )
-					brian_fantana( file )
-					printf "#{ file }\n"
+				begin
+					if file.match( StayClassy::Builder::VIEW_FILE_TYPES_REGEX )
+
+						# Brian Fantana for Sex Pantherization
+						brian_fantana( file )
+
+						# Champ Kind for sex and chicken
+						champ_kind( file )
+
+						# What does he do with a toaster again?
+						brick_tamland( file )
+
+						# Maybe next time wear a bra
+						veronica_corningstone( file )
+
+					end
+				rescue
+
 				end
   		end
   	end
@@ -32,19 +47,23 @@ module StayClassyProcess
 
 	# Slap some BBQ sauce on them elements and... Hawoooooooo, woo, woo, woo!!!!
 	def add_classes_to_file( file )
+		printf "#{ file }\n"
 	end
 	alias :champ_kind :add_classes_to_file
 
 	# Invite the ids to the pants party
 	def add_ids_to_file( file )
+		printf "#{ file }\n"
 	end
 	alias :brick_tamland :add_ids_to_file
 
 	# Thanks for listening, San Diego. And stay classy, San Diego. Thanks for listening, San Diego.
 	def save_new_file( file )
+		printf "#{ file }\n"
 	end
 	alias :veronica_corningstone :save_new_file
 
 	def baxter( file )
+		printf "#{ file }\n"
 	end
 end

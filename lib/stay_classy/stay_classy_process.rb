a1116848853749c487dbd25cc0f0961780661b85
Nick Mueller <nmueller@iexposure.com>
2014-09-18 13:19:23 -0500

    Refactor terminal output

--- lib/stay_classy/stay_classy_process.rb
+++ lib/stay_classy/stay_classy_process.rb
@@ -2,45 +2,49 @@ require 'nokogiri'
 
 module StayClassyProcess
 
-	# Once you've built an instance of stay_classy, douse it with Sex Panther.
+	# Once you've built an instance of stay_classy, it's time to hand it over to the 6 news team.
 	def process( stay_classy )
 
-		# if you need the views directory: StayClassy::Builder::VIEWS_DIR
-		prefix = stay_classy.instance_variable_get(:@prefix)
-		view_dirs = stay_classy.instance_variable_get(:@specified_view_directories)
+		prefix = stay_classy.instance_variable_get( :@prefix )
+		view_dirs = stay_classy.instance_variable_get( :@specified_view_directories )
 
-		printf "\n\n Directories to make classy with prefix #{ prefix }: \n".colorize( :yellow )
+		printf "\nDirectories to make classy with prefix #{ prefix }: \n".colorize( :yellow )
+		view_dirs.map { |vd| printf "#{ vd }\n" }
 
-		# Have Brian Fantana load those files.
+		# Send the right files to Brian Fantana for Sex Pantherization
+		printf "\nFiles to make classy: \n".colorize( :yellow )
 		view_dirs.each do |vd|
 			Dir.foreach( vd ) do |file|
 				if file.match( StayClassy::Builder::VIEW_FILE_TYPES_REGEX )
 					brian_fantana( file )
+					printf "#{ file }\n"
 				end
   		end
   	end
 
 	end
 
-	# Prepare the Sex Panther
-	def load_file( input )
-		doc = Nokogiri::HTML::Document.parse( input )
-		p input
+	# Get the bits of real panther, even if they smell like a turd covered in burnt hair
+	def load_file( file )
+		@doc = Nokogiri::HTML::Document.parse( file )
 	end
 	alias :brian_fantana :load_file
 
 	# Slap some BBQ sauce on them elements and... Hawoooooooo, woo, woo, woo!!!!
-	def add_classes_to_file
+	def add_classes_to_file( file )
 	end
 	alias :champ_kind :add_classes_to_file
 
 	# Invite the ids to the pants party
-	def add_ids_to_file
+	def add_ids_to_file( file )
 	end
-	alias :brick_tamlin :add_ids_to_file
+	alias :brick_tamland :add_ids_to_file
 
-	# Thanks for listening, San Diego
-	def save_new_file
+	# Thanks for listening, San Diego. And stay classy, San Diego. Thanks for listening, San Diego.
+	def save_new_file( file )
 	end
 	alias :veronica_corningstone :save_new_file
+
+	def baxter( file )
+	end
 end
